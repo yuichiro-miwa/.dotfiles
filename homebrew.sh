e94a114827281493541f774a29ab311f1e885705
三輪裕一郎 <kaminokobanzai@gmail.com>
2016-12-03 02:46:18 +0900

    test commit

---
+++ homebrew.sh
@@ -0,0 +1,89 @@
+#!/bin/sh
+
+# Check for Homebrew,Install if we don't have it
+if test ! $(which brew); then
+    echo "Installing homebrew..."
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update homebrew recipes
+printf "Update recipes? [Y/n]: " && read ANS
+if [ "${ANS}" = "Y" ]; then
+    brew update
+fi
+
+# Upgrade all
+printf "Upgrade? [Y/n]: " && read ANS
+if [ "${ANS}" = "Y" ]; then
+    brew upgrade
+fi
+
+packages=(
+
+    # Shell
+    zsh
+
+    # Git
+    git
+    tig
+
+    # Database
+
+    # Utils
+    openssl
+
+    # Tools
+    curl
+    nkf
+    tree
+    wget
+
+    # Languages
+    python3 # For use in Neovim
+
+)
+
+echo "installing binaries..."
+brew install ${packages[@]} && brew cleanup
+
+# Casks
+brew install caskroom/cask/brew-cask
+
+# Apps
+apps=(
+
+    # Launcher
+    alfred
+
+    # Browser
+    firefox
+    google-chrom
+
+    # Communication
+    slack
+    skype
+
+    # VM
+    virtualbox
+    vagrant
+
+    # etc ...
+    atom
+    genymotion
+    ### dropbox
+    ### google-drive
+    ### sketch
+
+    # development
+    iterm2
+    sourcetree
+    sequel-pro
+)
+
+# Install apps to /Applications
+echo "installing applications..."
+brew cask install ${apps[@]}
+
+# We need to link it
+brew cask alfred link
+
